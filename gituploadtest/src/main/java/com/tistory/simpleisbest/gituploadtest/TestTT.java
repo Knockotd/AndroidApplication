c88c2d90094b5caeeae040647e33c340ebb1706d
a503-26 <1>
2018-10-01 19:21:58 +0900

    1001 project

---
+++ gituploadtest/src/main/java/com/tistory/simpleisbest/gituploadtest/TestTT.java
@@ -0,0 +1,13 @@
+package com.tistory.simpleisbest.gituploadtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestTT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_tt);
+    }
+}
